d00da909298fb76a33b9b6020ae355443227ae87
lab04 <lab04@lab4-20>
2024-09-23 22:12:26 -0300

    Interface Criada

--- NBA/src/usa/com/nba/atletas/operacoesController.java
+++ NBA/src/usa/com/nba/atletas/operacoesController.java
@@ -1,5 +1,23 @@
 package usa.com.nba.atletas;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class operacoesController {
+	
+	@FXML
+	public TextField nome_input;
+	
+	@FXML
+	public TextField sobrenome_input;
+	
+	@FXML
+	public TextField idade_input;
+	
+	@FXML
+	public TextField posicao_input;
+	
+	@FXML
+	public TextField equipe_input;
 	
 }
